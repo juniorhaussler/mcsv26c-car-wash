bf821368dd60ddb3fae48f84b0ce1df63e10500b
juniorhaussler <juniorhaussler@gmail.com>
2023-05-07 23:56:19 -0300

    fix: correção de erros no componete completeForm.

--- src/components/completeForm/index.tsx
+++ src/components/completeForm/index.tsx
@@ -1,7 +1,11 @@
-import { useForm, FieldError } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+type Inputs = {
+  email: string,
+  password: string,
+};
 
 const schema = yup.object({
   email: yup.string().email("O email deve ter um formato válido").required("O campo email é necessário"),
@@ -10,14 +14,14 @@ const schema = yup.object({
 
 export default function CompleteForm() {
   
-  const {register, handleSubmit, watch, formState: { errors } } = useForm({
+  const {register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
     resolver: yupResolver(schema)});
   
-  const onSubmit = (data: any) => console.log(data);
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   console.log('errors', errors);
   return (
-    <div className="bg-purple-100 flex items-center justify-center w-">
+    <div className="bg-purple-100 flex items-center justify-center">
       <form onSubmit={ handleSubmit(onSubmit) } className=" bg-red-100 flex flex-col h-2/5  w-4/5">
         <label className=" flex flex-col">
           Email
